fdd3f037ee829e32102922060a150800dc9568f0
Tomás Lago <tomas.lago@mundo-r.com>
2022-01-22 21:24:41 +0100

    Apariencia searchBar

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,45 +3,52 @@ import './footer.css';
 
 const Footer = () => {
   return (
-      <footer className="footer">
-        <section className="newsletter">
-            <form className="sendEmail">
-              <h2>NEWSLETTER</h2>
-              <input 
-              type="text" 
-              id="email" 
-              name="email" 
-              placeholder="Email"></input>
-              <button 
-              className="enviar" 
-              type="submit">Enviar</button>
-            </form>
-            <form className="condition">
-            <input 
-            type="checkbox" 
-            id="politica" 
-            name="politica"></input>
-              <label for="politica">He leído y acepto la política de privacidad</label>
-            </form>
-        </section>
-        <section className="links">
-            <ul>
-              <li>
-                <a href="" target="blank">Condiciones de Uso</a>
-              </li>
-              <li>
-                <a href="" target="blank">Política de privacidad</a>
-              </li>
-              <li>
-                <a href="" target="blank">Contacto</a>
-              </li>
-              <li>
-                <a href="" target="blank">FAQ</a>
-              </li>
-            </ul>
-            </section>
-      
-      </footer>
+    <footer className="footer">
+      <section className="newsletter">
+        <form className="sendEmail">
+          <h2>NEWSLETTER</h2>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Email"
+          ></input>
+          <button className="enviar" type="submit">
+            Enviar
+          </button>
+        </form>
+        <form className="condition">
+          <input type="checkbox" id="politica" name="politica"></input>
+          <label htmlFor="politica">
+            He leído y acepto la política de privacidad
+          </label>
+        </form>
+      </section>
+      <section className="links">
+        <ul>
+          <li>
+            <a href="" target="blank">
+              Condiciones de Uso
+            </a>
+          </li>
+          <li>
+            <a href="" target="blank">
+              Política de privacidad
+            </a>
+          </li>
+          <li>
+            <a href="" target="blank">
+              Contacto
+            </a>
+          </li>
+          <li>
+            <a href="" target="blank">
+              FAQ
+            </a>
+          </li>
+        </ul>
+      </section>
+    </footer>
   );
 };
 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Background } from '../../components/background/Background';
 import Featured from '../../components/featured/Featured';
+import SearchBar from '../../components/searchBar/SearchBar';
 
 const Home = () => {
   return (
     <>
       <hr />
-      <Background />
+      <SearchBar />
       <hr />
       <Featured />
     </>
