3e236dee8751fe8e9222f8058df6a300829b892f
Tomás Lago <tomas.lago@mundo-r.com>
2022-01-22 23:01:46 +0100

    Arreglos CSS para móvil

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './footer.css';
 
 const Footer = () => {
+  const [input, setInput] = useState('');
+  const [checkbox, setCheckbox] = useState(false);
+
+  //Función que se encargará de introducir el correo
+
+  const inputHandle = (e) => {
+    setInput(e.target.value);
+  };
+
+  //Función que se encargará de validar el correo y
+  //enviar el formulario a la bbdd
+
+  const HandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (input) {
+      console.log(input);
+      //añadir a la base de datos.
+      setInput('');
+    }
+  };
+
+  const inputCheckbox = (e) => {
+    setCheckbox(!checkbox);
+    console.log(checkbox);
+  };
   return (
     <footer className="footer">
       <section className="newsletter">
-        <form className="sendEmail">
+        <form className="sendEmail" onSubmit={HandleSubmit}>
           <h2>NEWSLETTER</h2>
           <input
             type="text"
             id="email"
-            name="email"
-            placeholder="Email"
+            value={input}
+            onChange={inputHandle}
+            placeholder="example@gmail.com"
           ></input>
           <button className="enviar" type="submit">
             Enviar
           </button>
         </form>
         <form className="condition">
-          <input type="checkbox" id="politica" name="politica"></input>
+          <input
+            type="checkbox"
+            id="politica"
+            onChange={inputCheckbox}
+            value={checkbox}
+          ></input>
+
           <label htmlFor="politica">
             He leído y acepto la política de privacidad
           </label>
