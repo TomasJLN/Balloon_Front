d94524254f9e3ad63910009f6b34674765700de4
Tomás Lago <tomas.lago@mundo-r.com>
2022-01-23 11:23:36 +0100

    Fixed error folder Contact to contact

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Footer from './components/footer/Footer';
 import { Header } from './components/header/Header';
+import ContactForm from './forms/Contact_form/ContactForm';
 import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
-
       <Home />
+      <ContactForm />
       <Footer />
     </div>
   );
