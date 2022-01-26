d2da7b42bfa934b2f855542a7e42ea5df533923b
Tamara Raya <traya2387@gmail.com>
2022-01-26 22:38:47 +0100

    Modificado newsletter 26 enero

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,6 +5,15 @@ const Footer = () => {
   const [input, setInput] = useState('');
   const [checkbox, setCheckbox] = useState(false);
 
+
+  //Expresión regular para correo.
+  
+  const email = {
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+  };
+
+  //Función que crea una nueva newsletter.
+
   const createNewsletter = async (e) => {
     e.preventDefault();
 
@@ -13,8 +22,10 @@ const Footer = () => {
       headers:{
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({email, active}),
+      body: JSON.stringify({email}),
     });
+
+    console.log(res);
 
   if(res.ok){
     alert("Gracias por unirte a nuestras newsletter");
@@ -23,42 +34,68 @@ const Footer = () => {
   }
 };
 
+//Función que elimina una newsletter
+
+const deleteNewsletter = async (e) => {
+  e.preventDefault();
+
+  const res = await fetch("http://localhost:4000/newsletter", {
+    method: "DELETE",
+    headers:{
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({email}),
+  });
+
+if(res.ok){
+  alert("El correo asociado a la newsletter se eliminó");
+}else{
+  alert("Hubo un error al eliminar la newsletter. Inténtelo de nuevo");
+}
+};
 
   return (
     <footer className="footer">
       <section className="newsletter">
         <form className="sendEmail" onSubmit={createNewsletter}>
+          
           <h2>NEWSLETTER</h2>
+          
           <input
             type="text"
             id="email"
+            name="email"
+            regularPhrase={email.email}
             value={input}
             onChange={(e) => {
               setInput(e.target.value);
             }}
             placeholder="example@gmail.com"
+            error="El email debe contener un símbolo '@' y la terminación '.com'"
           ></input>
-          <button className="enviar" type="submit">
-            Enviar
+         
+          <button className="enviar" type="submit">Enviar
           </button>
         </form>
-
-        <form className="condition">
+        <form className="condition" onSubmit={createNewsletter}>
           <input
             type="checkbox"
             id="politica"
+            name="politica"
             value={checkbox}
             onChange={(e) => {
-              setCheckbox(e.target.value);
-            }}
-            
+              setCheckbox(e.target.checked);
+            }} 
+            error="Debes aceptar los términos y condiciones marcando la casilla de validación"
           ></input>
-
-          <label htmlFor="politica">
+            
+            <label htmlFor="politica">
             He leído y acepto la política de privacidad
           </label>
         </form>
-      </section>
+     
+     </section>
+
       <section className="links">
         <ul>
           <li>
