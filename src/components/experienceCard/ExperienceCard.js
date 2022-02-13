ce46fe94c65d74dfb485881518482c012e1a1e57
Tomás Lago <tomas.lago@mundo-r.com>
2022-02-13 14:05:21 +0100

    adaptado filtro y errores menores

--- src/components/experienceCard/ExperienceCard.js
+++ src/components/experienceCard/ExperienceCard.js
@@ -8,13 +8,6 @@ export const ExperienceCard = ({ exp }) => {
   const url = `https://www.google.es/maps/@${coords},19z`;
   const navigate = useNavigate();
 
-  console.log(
-    checkIfFileExists(
-      `${process.env.REACT_APP_BACKEND_URL}/uploads/${exp.photo}`
-    ),
-    `${process.env.REACT_APP_BACKEND_URL}/uploads/${exp.photo}`
-  );
-
   return (
     <div
       className="card fade_in"
