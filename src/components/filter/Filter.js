9d10732e6b5c9bfcfe2eab9ab64c5a4ba371970b
Tomás Lago <tomas.lago@mundo-r.com>
2022-02-28 10:16:28 +0100

    fix error filter

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,113 +1,107 @@
-import React, { useEffect, useState, useRef } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import { Formik, Form } from "formik";
-import queryString from "query-string";
-import SearchBar from "./SearchBar";
-import RatingSearch from "./RatingSearch";
-import DateSearch from "./DateSearch";
-import CategorySearch from "./CategorySearch";
-import LocationSearch from "./LocationSearch";
-import PriceSearch from "./PriceSearch";
-import "./filter.css";
-import "react-multi-date-picker/styles/layouts/mobile.css";
+import React, { useEffect, useState, useRef } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { Formik, Form } from 'formik';
+import queryString from 'query-string';
+import SearchBar from './SearchBar';
+import RatingSearch from './RatingSearch';
+import DateSearch from './DateSearch';
+import CategorySearch from './CategorySearch';
+import LocationSearch from './LocationSearch';
+import PriceSearch from './PriceSearch';
+import './filter.css';
+import 'react-multi-date-picker/styles/layouts/mobile.css';
 
 const Filter = () => {
-	const datePickerRef = useRef();
+  const datePickerRef = useRef();
 
-	const navigate = useNavigate();
-	const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-	let { experience } = queryString.parse(location.search);
-	experience = experience ? experience : "";
+  let { experience } = queryString.parse(location.search);
+  experience = experience ? experience : '';
 
-	const [rating, setRating] = useState("");
-	const [searchCat, setSearchCat] = useState("");
-	const [searchLoc, setSearchLoc] = useState("");
-	const [searchPrice, setSearchPrice] = useState([0, 1000]);
+  const [rating, setRating] = useState('');
+  const [searchCat, setSearchCat] = useState('');
+  const [searchLoc, setSearchLoc] = useState('');
+  const [searchPrice, setSearchPrice] = useState([0, 1000]);
 
-	const [searchDate, setSearchDate] = useState("");
-	const [toSearch, setToSearch] = useState(experience ? experience : "");
+  const [searchDate, setSearchDate] = useState('');
+  const [toSearch, setToSearch] = useState(experience ? experience : '');
 
-	useEffect(() => {
-		if (location.pathname === "/") resetInput();
-	}, [location.pathname]);
+  useEffect(() => {
+    if (location.pathname === '/') resetInput();
+  }, [location.pathname]);
 
-	const resetInput = () => {
-		setToSearch("");
-		setSearchCat("");
-		setSearchLoc("");
-		setSearchStartPrice("");
-		setSearchDate("");
-		setRating("");
-	};
+  const resetInput = () => {
+    setToSearch('');
+    setSearchCat('');
+    setSearchLoc('');
+    setSearchPrice('');
+    setSearchDate('');
+    setRating('');
+  };
 
-	const handleSubmit = (e) => {
-		toSearch && navigate(`/allFilter?experience=${toSearch}`);
-		e.preventDefault();
+  const handleSubmit = (e) => {
+    toSearch && navigate(`/allFilter?experience=${toSearch}`);
+    e.preventDefault();
 
-		resetInput();
-	};
+    resetInput();
+  };
 
-	useEffect(() => {
-		let query = `/allFilter?experience=${experience}`;
-		query += searchPrice[0] ? `&start_price=${searchPrice[0]}` : "";
-		query += searchPrice[1] ? `&end_price=${searchPrice[1]}` : "";
-		query += searchCat ? `&category=${searchCat}` : "";
-		query += searchLoc ? `&location=${searchLoc}` : "";
-		query += searchDate ? `&start=${searchDate[0]}` : "";
-		query += searchDate.length > 1 ? `&end=${searchDate[1]}` : "";
-		query += rating ? `&review?searchByExp=${rating}` : "";
-		toSearch && navigate(`/allFilter?experience=${toSearch}`);
+  useEffect(() => {
+    let query = `/allFilter?experience=${experience}`;
+    query += searchPrice[0] ? `&start_price=${searchPrice[0]}` : '';
+    query += searchPrice[1] ? `&end_price=${searchPrice[1]}` : '';
+    query += searchCat ? `&category=${searchCat}` : '';
+    query += searchLoc ? `&location=${searchLoc}` : '';
+    query += searchDate ? `&start=${searchDate[0]}` : '';
+    query += searchDate.length > 1 ? `&end=${searchDate[1]}` : '';
+    query += rating ? `&review?searchByExp=${rating}` : '';
+    toSearch && navigate(`/allFilter?experience=${toSearch}`);
 
-		navigate(query);
+    navigate(query);
+  }, [searchCat, searchLoc, searchPrice, experience, searchDate, rating]);
+  console.log('SEARCH PRICE', searchPrice);
 
-	}, [searchCat, searchLoc, searchPrice, experience, searchDate, rating]);
-	console.log("SEARCH PRICE", searchPrice);
+  return (
+    <>
+      <Formik>
+        {() => (
+          <Form onSubmit={handleSubmit}>
+            <div className="hero">
+              <div className="searchContainer">
+                <SearchBar toSearch={toSearch} setToSearch={setToSearch} />
 
+                <DateSearch
+                  datePickerRef={datePickerRef}
+                  searchDate={searchDate}
+                  setSearchDate={setSearchDate}
+                />
+              </div>
+            </div>
 
+            <div className="filterContainer">
+              <CategorySearch
+                searchCat={searchCat}
+                setSearchCat={setSearchCat}
+              />
+              <LocationSearch
+                searchLoc={searchLoc}
+                setSearchLoc={setSearchLoc}
+              />
 
-	return (
-		<>
-			<Formik>
-				{() => (
-					<Form onSubmit={handleSubmit}>
-						<div className="hero">
-							<div className="searchContainer">
-								<SearchBar toSearch={toSearch} setToSearch={setToSearch} />
+              <PriceSearch
+                searchPrice={searchPrice}
+                setSearchPrice={setSearchPrice}
+              />
 
-								<DateSearch
-									datePickerRef={datePickerRef}
-									searchDate={searchDate}
-									setSearchDate={setSearchDate}
-								/>
-							</div>
-						</div>
-
-						<div className="filterContainer">
-
-							<CategorySearch
-								searchCat={searchCat}
-								setSearchCat={setSearchCat}
-							/>
-							<LocationSearch
-								searchLoc={searchLoc}
-								setSearchLoc={setSearchLoc}
-							/>
-
-							<PriceSearch
-								searchPrice={searchPrice}
-								setSearchPrice={setSearchPrice}
-							/>
-
-
-								<RatingSearch rating={rating} setRating={setRating} />
-							</div>
-						</div>
-					</Form>
-				)}
-			</Formik>
-		</>
-	);
+              <RatingSearch rating={rating} setRating={setRating} />
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
 };
 
 export default Filter;
