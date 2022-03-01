893b3b5e7f01c90fbaecf70556404312443ef73a
Daniel Pereira <danipereiracodes@mac.local>
2022-03-02 00:40:46 +0100

    reset filter on logo click

--- src/components/filter/PriceSearch.js
+++ src/components/filter/PriceSearch.js
@@ -5,12 +5,17 @@ import Slider from "@mui/material/Slider";
 const PriceSearch = ({ setSearchPrice, searchPrice }) => {
 	return (
 		<div className="priceSearch">
+			{searchPrice[0] <= 1 && searchPrice[1] >= 1000 ? (
+				<p>Filtrar por precio</p>
+			) : (
+				<p>{`${searchPrice[0]}€ - ${searchPrice[1]}€`}</p>
+			)}
 			<Slider
 				color="secondary"
 				sx={{ width: "200px" }}
 				min={0}
 				max={1000}
-				step={100}
+				step={25}
 				value={searchPrice}
 				onChange={(e) => {
 					setSearchPrice(e.target.value);
@@ -18,7 +23,6 @@ const PriceSearch = ({ setSearchPrice, searchPrice }) => {
 				}}
 				valueLabelDisplay="auto"
 			/>
-			{searchPrice && <p>{`${searchPrice[0]}€ - ${searchPrice[1]}€`}</p>}
 		</div>
 	);
 };
