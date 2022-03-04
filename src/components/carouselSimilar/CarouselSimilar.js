94bbb31827b489ceff526cd6bba7c519a676096f
TomasJLN <tomas.lago@mundo-r.com>
2022-03-04 14:22:02 +0100

    swipper no log

--- src/components/carouselSimilar/CarouselSimilar.js
+++ src/components/carouselSimilar/CarouselSimilar.js
@@ -52,8 +52,8 @@ export const CarouselSimilar = ({ reviews, avgRatin, idCategory = 1 }) => {
           },
         }}
         spaceBetween={15}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
+        // onSlideChange={() => console.log("slide change")}
+        // onSwiper={(swiper) => console.log(swiper)}
       >
         {filterExp.map((exp) => (
           <SwiperSlide key={exp.title}>
