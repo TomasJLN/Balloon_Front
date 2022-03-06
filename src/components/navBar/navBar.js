ee71b64427ca3170387d3ddf7ea55d43ca26d3fb
TomasJLN <tomas.lago@mundo-r.com>
2022-03-06 12:08:02 +0100

    links a categorías en NavBar

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,53 +1,51 @@
 import { Link } from "react-router-dom";
 import { useEffect, useRef } from "react";
-import Dropdown from '../dropDown/DropDown';
+import Dropdown from "../dropDown/DropDown";
 import "./navBar.css";
 
 const NavBar = ({ setShowNavBar }) => {
-	const ref = useRef(null);
+  const ref = useRef(null);
 
-	useEffect(() => {
-		const handleClickOutside = (e) => {
-			if (ref.current && !ref.current.contains(e.target)) {
-				e.stopPropagation();
-				setShowNavBar((s) => !s);
-			}
-		};
-		document.addEventListener("click", handleClickOutside, true);
-		return () => {
-			document.removeEventListener("click", handleClickOutside, true);
-		};
-	}, [setShowNavBar]);
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        e.stopPropagation();
+        setShowNavBar((s) => !s);
+      }
+    };
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [setShowNavBar]);
 
-	return (
-		<menu ref={ref} className="navbar">
-			<li className="dropdown-btn" >
-				<Link  to="/" onClick={(s) => setShowNavBar(!s)}>
-					INICIO
-				</Link>
-			</li>
-			<li className="dropdown-btn" >
-				<Link to="/account" onClick={(s) => setShowNavBar(!s)}>
-					MI CUENTA
-				</Link>
-			</li>
-			<Dropdown/>  
-			<li className="dropdown-btn" >
-				<Link to="/contact-form" onClick={(s) => setShowNavBar(!s)}>
-					CONTACTO
-				</Link>
-			</li>
-			<li className="dropdown-btn" >
-				<Link to="/faq" onClick={(s) => setShowNavBar(!s)}>
-					FAQ
-				</Link>
-			</li>
-		</menu>
-	);
+  return (
+    <menu ref={ref} className="navbar">
+      <li className="dropdown-btn">
+        <Link to="/" onClick={(s) => setShowNavBar(!s)}>
+          INICIO
+        </Link>
+      </li>
+      <li className="dropdown-btn">
+        <Link to="/account" onClick={(s) => setShowNavBar(!s)}>
+          MI CUENTA
+        </Link>
+      </li>
+      {/* Añadimos al componente dropDown la función para cambiar el valor de showNavBar (setShowNavBar)
+		  para poder cerrar el menú cuando hagamos click en una categoría */}
+      <Dropdown setShowNavBar={setShowNavBar} />
+      <li className="dropdown-btn">
+        <Link to="/contact-form" onClick={(s) => setShowNavBar(!s)}>
+          CONTACTO
+        </Link>
+      </li>
+      <li className="dropdown-btn">
+        <Link to="/faq" onClick={(s) => setShowNavBar(!s)}>
+          FAQ
+        </Link>
+      </li>
+    </menu>
+  );
 };
 
 export default NavBar;
-
-
- 
- 
