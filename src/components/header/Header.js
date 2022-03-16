e3f90f22219bf03635dbaccdc0e3528522cbbc71
Tomás Lago <tomas.lago@mundo-r.com>
2022-03-16 17:12:05 +0100

    close button header fixed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -13,83 +13,80 @@ import NavUser from "../navUser/NavUser";
 import "./header.css";
 
 export const Header = ({
-	toSearch,
-	setToSearch,
-	toSearchTit,
-	setToSearchTit,
-	searchCat,
-	setSearchCat,
+  toSearch,
+  setToSearch,
+  toSearchTit,
+  setToSearchTit,
+  searchCat,
+  setSearchCat,
 }) => {
-	const [showNavBar, setShowNavBar] = useState(false);
-	const [userMenu, setUserMenu] = useState(false);
-	const [token, setToken] = useContext(TokenContext);
-	const [usuario, setUsuario] = useContext(UserContext);
-	const [error, setError] = useState(null);
-	const [loading, setLoading] = useState(false);
+  const [showNavBar, setShowNavBar] = useState(false);
+  const [userMenu, setUserMenu] = useState(false);
+  const [token, setToken] = useContext(TokenContext);
+  const [usuario, setUsuario] = useContext(UserContext);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		if (token && token !== "")
-			fetcher(setUsuario, setError, setLoading, "user", {
-				headers: {
-					Authorization: token,
-				},
-			});
-	}, [token, setUsuario]);
+  useEffect(() => {
+    if (token && token !== "")
+      fetcher(setUsuario, setError, setLoading, "user", {
+        headers: {
+          Authorization: token,
+        },
+      });
+  }, [token, setUsuario]);
 
-	const refreshPage = () => {
-		usuario.role === "admin" && navigate("/");
-		window.location.reload(false);
-	};
+  const refreshPage = () => {
+    usuario.role === "admin" && navigate("/");
+    window.location.reload(false);
+  };
 
-	const handleClick = () => {
-		navigate("/");
-		refreshPage();
-	};
+  const handleClick = () => {
+    navigate("/");
+    refreshPage();
+  };
 
-	return (
-		<>
-			{loading ? (
-				<h1>Loading...</h1>
-			) : (
-				<header id="main_header">
-					<nav>
-						{showNavBar && (
-							<NavBar
-								toSearch={toSearch}
-								setToSearch={setToSearch}
-								toSearchTit={toSearchTit}
-								setToSearchTit={setToSearchTit}
-								searchCat={searchCat}
-								setSearchCat={setSearchCat}
-								setShowNavBar={setShowNavBar}
-							/>
-						)}
+  return (
+    <>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <header id="main_header">
+          <nav>
+            {showNavBar && (
+              <NavBar
+                toSearch={toSearch}
+                setToSearch={setToSearch}
+                toSearchTit={toSearchTit}
+                setToSearchTit={setToSearchTit}
+                searchCat={searchCat}
+                setSearchCat={setSearchCat}
+                setShowNavBar={setShowNavBar}
+              />
+            )}
 
-						{!showNavBar ? (
-							<FaBars
-								className="menuprincipal"
-								onClick={() => {
-									setShowNavBar(!showNavBar);
-								}}
-							/>
-						) : (
-							<GrClose
-							/>
-						)}
-					</nav>
-					<GiBalloonDog style={{ fontSize: "60px" }} onClick={handleClick} />
-					<div>
-						{userMenu && (
-							<NavUser setUserMenu={setUserMenu} usuario={usuario} />
-						)}
-						<Avatar 
-						
-						usuario={usuario} setUserMenu={setUserMenu} />
-					</div>
-				</header>
-			)}
-		</>
-	);
+            {!showNavBar ? (
+              <FaBars
+                className="menuprincipal"
+                onClick={() => {
+                  setShowNavBar(!showNavBar);
+                }}
+              />
+            ) : (
+              <GrClose className="menuprincipal" />
+            )}
+          </nav>
+          <GiBalloonDog style={{ fontSize: "60px" }} onClick={handleClick} />
+          <div>
+            {userMenu && (
+              <NavUser setUserMenu={setUserMenu} usuario={usuario} />
+            )}
+            <Avatar usuario={usuario} setUserMenu={setUserMenu} />
+          </div>
+        </header>
+      )}
+    </>
+  );
 };
