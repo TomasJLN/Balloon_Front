aee4747dea11a1ce818249d61b36cdd2f8805bd4
TomasJLN <tomas.lago@mundo-r.com>
2022-03-17 09:04:46 +0100

    no erros please

--- src/pages/experience/Experience.js
+++ src/pages/experience/Experience.js
@@ -13,157 +13,157 @@ import "./experience.css";
 import moment from "moment";
 
 const Experience = () => {
-	const { id } = useParams();
+  const { id } = useParams();
 
-	const {
-		idCategory,
-		title,
-		description,
-		price,
-		location,
-		coords,
-		photo,
-		endDate,
+  const {
+    idCategory,
+    title,
+    description,
+    price,
+    location,
+    coords,
+    photo,
+    endDate,
 
-		startDate,
-		conditions,
-		normatives,
-	} = useExperience(id);
+    startDate,
+    conditions,
+    normatives,
+  } = useExperience(id);
 
-	let url = `https://www.google.es/maps/@${coords},19z`;
+  let url = `https://www.google.es/maps/@${coords},19z`;
 
-	url = url.replace(/ +/g, "");
+  url = url.replace(/ +/g, "");
 
-	let infoExperience = [];
-	infoExperience.push({ title: "Condiciones", content: conditions });
-	infoExperience.push({ title: "Normativas", content: normatives });
+  let infoExperience = [];
+  infoExperience.push({ title: "Condiciones", content: conditions });
+  infoExperience.push({ title: "Normativas", content: normatives });
 
-	const navigate = useNavigate();
-	const { reviews, error, loading } = useGetReviews(id);
-	const [avgRatin, setAvgRatin] = useState(0);
+  const navigate = useNavigate();
+  const { reviews, error, loading } = useGetReviews(id);
+  const [avgRatin, setAvgRatin] = useState(0);
 
-	useEffect(() => {
-		reviews.length !== 0 &&
-			setAvgRatin(
-				reviews.reduce((acc, exp) => acc + exp.score, 0) / reviews.length
-			);
-		reviews.length === 0 && setAvgRatin(0);
-	}, [reviews]);
+  useEffect(() => {
+    reviews.length !== 0 &&
+      setAvgRatin(
+        reviews.reduce((acc, exp) => acc + exp.score, 0) / reviews.length
+      );
+    reviews.length === 0 && setAvgRatin(0);
+  }, [reviews]);
 
-	useEffect(() => {
-		scrollToTop();
-	}, [id]);
+  useEffect(() => {
+    scrollToTop();
+  }, [id]);
 
-	return (
-		<div className="exp-container">
-			<div className="rating-back">
-				{avgRatin > 0 && (
-					<>
-						<Rating
-							ratingValue={avgRatin}
-							size="16px"
-							tooltipClassName="stars-count"
-							readonly={true}
-						/>
-						<span className="counter-reviews">({reviews.length})</span>
-					</>
-				)}
-			</div>
-			<div className="figure-description-container">
-				<div className="figure-container">
-					<div className="buttoncontainer">
-						<button
-							className="buttonback"
-							onClick={() => {
-								navigate(-1);
-							}}
-						>
-							Volver
-						</button>
-					</div>
-					<figure>
-						{photo ? (
-							<img
-								src={`${process.env.REACT_APP_BACKEND_URL}/uploads/${photo}`}
-								alt={title}
-								className="exp-picture"
-							/>
-						) : (
-							<img
-								src={`${process.env.REACT_APP_BACKEND_URL}/uploads/NA.png`}
-								alt={title}
-								className="exp-picture"
-							/>
-						)}
-					</figure>
-				</div>
+  return (
+    <div className="exp-container">
+      <div className="rating-back">
+        {avgRatin > 0 && (
+          <>
+            <Rating
+              ratingValue={avgRatin}
+              size="16px"
+              tooltipClassName="stars-count"
+              readonly={true}
+            />
+            <span className="counter-reviews">({reviews.length})</span>
+          </>
+        )}
+      </div>
+      <div className="figure-description-container">
+        <div className="figure-container">
+          <div className="buttoncontainer">
+            <button
+              className="buttonback"
+              onClick={() => {
+                navigate(-1);
+              }}
+            >
+              Volver
+            </button>
+          </div>
+          <figure>
+            {photo ? (
+              <img
+                src={`${process.env.REACT_APP_BACKEND_URL}/uploads/${photo}`}
+                alt={title}
+                className="exp-picture"
+              />
+            ) : (
+              <img
+                src={`${process.env.REACT_APP_BACKEND_URL}/uploads/NA.png`}
+                alt={title}
+                className="exp-picture"
+              />
+            )}
+          </figure>
+        </div>
 
-				<div className="description-container">
-					<h1 id="exp-title">{title}</h1>
-					<div className="exp-description">
-						<p>{description}</p>
-					</div>
-					<div>
-						<p className="exp-date">
-							<a
-								href={url}
-								target="_blank"
-								rel="noreferrer noopener"
-								className="card-location"
-							>
-								{" "}
-								<p>
-									<MdLocationPin className="icon-search" />
-									En <strong>{location}</strong> desde{" "}
-									<strong>{moment(startDate).format("DD-MM-YYYY")}</strong>{" "}
-									hasta <strong>{moment(endDate).format("DD-MM-YYYY")}</strong>
-								</p>
-							</a>
-						</p>
-					</div>
+        <div className="description-container">
+          <h1 id="exp-title">{title}</h1>
+          <div className="exp-description">
+            <p>{description}</p>
+          </div>
+          <div>
+            <p className="exp-date">
+              <a
+                href={url}
+                target="_blank"
+                rel="noreferrer noopener"
+                className="card-location"
+              >
+                {" "}
+                <span>
+                  <MdLocationPin className="icon-search" />
+                  En <strong>{location}</strong> desde{" "}
+                  <strong>{moment(startDate).format("DD-MM-YYYY")}</strong>{" "}
+                  hasta <strong>{moment(endDate).format("DD-MM-YYYY")}</strong>
+                </span>
+              </a>
+            </p>
+          </div>
 
-					<div className="ratin-comprar">
-						<p id="precio-exp">{price} €</p>
-						<button
-							className="buy-button"
-							onClick={(e) => {
-								navigate(`/booking/${id}`);
-							}}
-						>
-							Comprar
-						</button>
-					</div>
-				</div>
-			</div>
+          <div className="ratin-buy-button">
+            <p id="precio-exp">{price} €</p>
+            <button
+              className="buy-button"
+              onClick={(e) => {
+                navigate(`/booking/${id}`);
+              }}
+            >
+              Comprar
+            </button>
+          </div>
+        </div>
+      </div>
 
-			{infoExperience.map(({ title, content }) => (
-				<Accordion
-					className="accordion-section"
-					key={title}
-					title={title}
-					content={content}
-				/>
-			))}
+      {infoExperience.map(({ title, content }) => (
+        <Accordion
+          className="accordion-section"
+          key={title}
+          title={title}
+          content={content}
+        />
+      ))}
 
-			<hr id="opinions-section" />
-			{avgRatin !== 0 && <Reviews id={id} reviews={reviews} />}
-			<hr />
+      <hr id="opinions-section" />
+      {avgRatin !== 0 && <Reviews id={id} reviews={reviews} />}
+      <hr />
 
-			<Mapa photo={photo} title={title} coords={coords} url={url} />
+      <Mapa photo={photo} title={title} coords={coords} url={url} />
 
-			<div>
-				<h2 id="ex-sim">Otras experiencias que podrían interesarte</h2>
-				<div>
-					<CarouselSimilar
-						id={id}
-						reviews={reviews}
-						avgRatin={avgRatin}
-						idCategory={idCategory}
-					/>
-				</div>
-			</div>
-		</div>
-	);
+      <div>
+        <h2 id="ex-sim">Otras experiencias que podrían interesarte</h2>
+        <div>
+          <CarouselSimilar
+            id={id}
+            reviews={reviews}
+            avgRatin={avgRatin}
+            idCategory={idCategory}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
