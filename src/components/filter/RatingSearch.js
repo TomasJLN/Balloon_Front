d953a4f5e3c1622726c9fd3208869ffb37922c5b
Daniel Pereira <danipereiracodes@mac.local>
2022-03-21 14:15:16 +0100

    fixed slider range and rating style

--- src/components/filter/RatingSearch.js
+++ src/components/filter/RatingSearch.js
@@ -4,13 +4,13 @@ const RatingSearch = ({ rating, setRating }) => {
 	return (
 		<div className="ratingSearch">
 			<p>Valoración</p>
-			<div className="generalFilter">
+			<div>
 				<Rating
 					fillColor="rgb(var(--primary-color-hard))"
 					tooltipDefaultText="Por puntos"
 					onClick={setRating}
 					ratingValue={rating}
-					size={25}
+					size={20}
 				/>
 			</div>
 		</div>
